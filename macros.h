6fdc1bab927b8b103f3d7a5ba32328c2fe05f0ac
nikoehler <nikoehler@deloitte.de>
2025-03-04 16:41:24 +0100

    Counting correctly...

--- macros.h
+++ macros.h
@@ -32,8 +32,8 @@
 /* GAME MOVE EXPLANATION */
 
 /*
-0000 0000 0000 0000 0000 0111 1111 -> From which square came the Move (0x3F)
-0000 0000 0000 0011 1111 0000 0000 -> To which square goes the move (>>7, 0x3F)
+0000 0000 0000 0000 0000 0111 1111 -> From which square came the Move (0x7F)
+0000 0000 0000 0011 1111 0000 0000 -> To which square goes the move (>>7, 0x7F)
 0000 0000 0011 1100 0000 0000 0000 -> Which (if any) piece we capture (>>14, 0xF)
 0000 0000 0100 0000 0000 0000 0000 -> Is it En Passant Move (0x40000)
 0000 0000 1000 0000 0000 0000 0000 -> Is it the first move a pawn did? (0x80000)
@@ -41,8 +41,8 @@
 0001 0000 0000 0000 0000 0000 0000 -> Did we castle? (0x1000000)
 */
 
-#define FROMSQ(m) ((m) & 0x3F)
-#define TOSQ(m) (((m) >> 7) & 0x3F)
+#define FROMSQ(m) ((m) & 0x7F)
+#define TOSQ(m) (((m) >> 7) & 0x7F)
 #define CAPTURED(m) (((m) >> 14) & 0xF)
 #define PROMOTED(m) (((m) >> 20) & 0xF)
 
