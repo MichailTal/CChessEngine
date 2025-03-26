3aee3894f9b736dc5cf30711c554ae0880bdec58
nikoehler <nikoehler@deloitte.de>
2025-03-26 17:18:19 +0100

    rm wrong FEN for testing

--- src/cchess_engine.c
+++ src/cchess_engine.c
@@ -7,9 +7,6 @@
 #include "stdlib.h"
 #include "string.h"
 
-#define HARDTEST                                                               \
-  "r2k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3RK2R w KQkq - 0 1"
-
 #define WAC1                                                                   \
   "r1b1k2r/ppppnppp/2n2q2/2b5/3NP3/2P1B3/PP3PPP/RN1QKB1R w KQkq - 0 1"
 
