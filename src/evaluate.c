bef55568323983b81f593ce4dafda803ed482394
nikoehler <nikoehler@deloitte.de>
2025-04-10 14:15:26 +0200

    Include Material Draw

--- src/evaluate.c
+++ src/evaluate.c
@@ -13,6 +13,10 @@ const int QueenOpenFile = 5;
 const int QueenSemiOpenFile = 3;
 const int BishopPair = 30;
 
+#define ENDGAME_MAT                                                            \
+  (1 * PieceValue[wR] + 2 * PieceValue[wN] + 2 * PieceValue[wP] +              \
+   PieceValue[wK])
+
 const int PawnTable[64] = {0,  0,  0,  0,  0,  0,  0,  0,  10, 10, 0,  -10, -10,
                            0,  10, 10, 5,  0,  0,  5,  5,  0,  0,  5,  0,   0,
                            10, 20, 20, 10, 0,  0,  5,  5,  5,  10, 10, 5,   5,
@@ -56,23 +60,67 @@ int Mirror64[64] = {56, 57, 58, 59, 60, 61, 62, 63, 48, 49, 50, 51, 52,
                     31, 16, 17, 18, 19, 20, 21, 22, 23, 8,  9,  10, 11,
                     12, 13, 14, 15, 0,  1,  2,  3,  4,  5,  6,  7};
 
+int MaterialDraw(const board_representation *pos) {
+
+  ASSERT(CheckBoard(pos));
+
+  if (!pos->pieceNumber[wR] && !pos->pieceNumber[bR] && !pos->pieceNumber[wQ] &&
+      !pos->pieceNumber[bQ]) {
+    if (!pos->pieceNumber[bB] && !pos->pieceNumber[wB]) {
+      if (pos->pieceNumber[wN] < 3 && pos->pieceNumber[bN] < 3) {
+        return TRUE;
+      }
+    } else if (!pos->pieceNumber[wN] && !pos->pieceNumber[bN]) {
+      if (abs(pos->pieceNumber[wB] - pos->pieceNumber[bB]) < 2) {
+        return TRUE;
+      }
+    } else if ((pos->pieceNumber[wN] < 3 && !pos->pieceNumber[wB]) ||
+               (pos->pieceNumber[wB] == 1 && !pos->pieceNumber[wN])) {
+      if ((pos->pieceNumber[bN] < 3 && !pos->pieceNumber[bB]) ||
+          (pos->pieceNumber[bB] == 1 && !pos->pieceNumber[bN])) {
+        return TRUE;
+      }
+    }
+  } else if (!pos->pieceNumber[wQ] && !pos->pieceNumber[bQ]) {
+    if (pos->pieceNumber[wR] == 1 && pos->pieceNumber[bR] == 1) {
+      if ((pos->pieceNumber[wN] + pos->pieceNumber[wB]) < 2 &&
+          (pos->pieceNumber[bN] + pos->pieceNumber[bB]) < 2) {
+        return TRUE;
+      }
+    } else if (pos->pieceNumber[wR] == 1 && !pos->pieceNumber[bR]) {
+      if ((pos->pieceNumber[wN] + pos->pieceNumber[wB] == 0) &&
+          (((pos->pieceNumber[bN] + pos->pieceNumber[bB]) == 1) ||
+           ((pos->pieceNumber[bN] + pos->pieceNumber[bB]) == 2))) {
+        return TRUE;
+      }
+    } else if (pos->pieceNumber[bR] == 1 && !pos->pieceNumber[wR]) {
+      if ((pos->pieceNumber[bN] + pos->pieceNumber[bB] == 0) &&
+          (((pos->pieceNumber[wN] + pos->pieceNumber[wB]) == 1) ||
+           ((pos->pieceNumber[wN] + pos->pieceNumber[wB]) == 2))) {
+        return TRUE;
+      }
+    }
+  }
+  return FALSE;
+}
+
 int EvalPosition(const board_representation *pos) {
 
   ASSERT(CheckBoard(pos));
 
   int pce;
-  int pceNum;
+  int pieceNumber;
   int sq;
   int score = pos->material[WHITE] - pos->material[BLACK];
 
-  // if (!pos->pieceNumber[wP] && !pos->pieceNumber[bP] &&
-  //     MaterialDraw(pos) == TRUE) {
-  //   return 0;
-  // }
+  if (!pos->pieceNumber[wP] && !pos->pieceNumber[bP] &&
+      MaterialDraw(pos) == TRUE) {
+    return 0;
+  }
 
   pce = wP;
-  for (pceNum = 0; pceNum < pos->pieceNumber[pce]; ++pceNum) {
-    sq = pos->pieceList[pce][pceNum];
+  for (pieceNumber = 0; pieceNumber < pos->pieceNumber[pce]; ++pieceNumber) {
+    sq = pos->pieceList[pce][pieceNumber];
     ASSERT(SqOnBoard(sq));
     ASSERT(SQ64(sq) >= 0 && SQ64(sq) <= 63);
     score += PawnTable[SQ64(sq)];
@@ -87,8 +135,8 @@ int EvalPosition(const board_representation *pos) {
   }
 
   pce = bP;
-  for (pceNum = 0; pceNum < pos->pieceNumber[pce]; ++pceNum) {
-    sq = pos->pieceList[pce][pceNum];
+  for (pieceNumber = 0; pieceNumber < pos->pieceNumber[pce]; ++pieceNumber) {
+    sq = pos->pieceList[pce][pieceNumber];
     ASSERT(SqOnBoard(sq));
     ASSERT(MIRROR64(SQ64(sq)) >= 0 && MIRROR64(SQ64(sq)) <= 63);
     score -= PawnTable[MIRROR64(SQ64(sq))];
@@ -103,40 +151,40 @@ int EvalPosition(const board_representation *pos) {
   }
 
   pce = wN;
-  for (pceNum = 0; pceNum < pos->pieceNumber[pce]; ++pceNum) {
-    sq = pos->pieceList[pce][pceNum];
+  for (pieceNumber = 0; pieceNumber < pos->pieceNumber[pce]; ++pieceNumber) {
+    sq = pos->pieceList[pce][pieceNumber];
     ASSERT(SqOnBoard(sq));
     ASSERT(SQ64(sq) >= 0 && SQ64(sq) <= 63);
     score += KnightTable[SQ64(sq)];
   }
 
   pce = bN;
-  for (pceNum = 0; pceNum < pos->pieceNumber[pce]; ++pceNum) {
-    sq = pos->pieceList[pce][pceNum];
+  for (pieceNumber = 0; pieceNumber < pos->pieceNumber[pce]; ++pieceNumber) {
+    sq = pos->pieceList[pce][pieceNumber];
     ASSERT(SqOnBoard(sq));
     ASSERT(MIRROR64(SQ64(sq)) >= 0 && MIRROR64(SQ64(sq)) <= 63);
     score -= KnightTable[MIRROR64(SQ64(sq))];
   }
 
   pce = wB;
-  for (pceNum = 0; pceNum < pos->pieceNumber[pce]; ++pceNum) {
-    sq = pos->pieceList[pce][pceNum];
+  for (pieceNumber = 0; pieceNumber < pos->pieceNumber[pce]; ++pieceNumber) {
+    sq = pos->pieceList[pce][pieceNumber];
     ASSERT(SqOnBoard(sq));
     ASSERT(SQ64(sq) >= 0 && SQ64(sq) <= 63);
     score += BishopTable[SQ64(sq)];
   }
 
   pce = bB;
-  for (pceNum = 0; pceNum < pos->pieceNumber[pce]; ++pceNum) {
-    sq = pos->pieceList[pce][pceNum];
+  for (pieceNumber = 0; pieceNumber < pos->pieceNumber[pce]; ++pieceNumber) {
+    sq = pos->pieceList[pce][pieceNumber];
     ASSERT(SqOnBoard(sq));
     ASSERT(MIRROR64(SQ64(sq)) >= 0 && MIRROR64(SQ64(sq)) <= 63);
     score -= BishopTable[MIRROR64(SQ64(sq))];
   }
 
   pce = wR;
-  for (pceNum = 0; pceNum < pos->pieceNumber[pce]; ++pceNum) {
-    sq = pos->pieceList[pce][pceNum];
+  for (pieceNumber = 0; pieceNumber < pos->pieceNumber[pce]; ++pieceNumber) {
+    sq = pos->pieceList[pce][pieceNumber];
     ASSERT(SqOnBoard(sq));
     ASSERT(SQ64(sq) >= 0 && SQ64(sq) <= 63);
     score += RookTable[SQ64(sq)];
@@ -151,8 +199,8 @@ int EvalPosition(const board_representation *pos) {
   }
 
   pce = bR;
-  for (pceNum = 0; pceNum < pos->pieceNumber[pce]; ++pceNum) {
-    sq = pos->pieceList[pce][pceNum];
+  for (pieceNumber = 0; pieceNumber < pos->pieceNumber[pce]; ++pieceNumber) {
+    sq = pos->pieceList[pce][pieceNumber];
     ASSERT(SqOnBoard(sq));
     ASSERT(MIRROR64(SQ64(sq)) >= 0 && MIRROR64(SQ64(sq)) <= 63);
     score -= RookTable[MIRROR64(SQ64(sq))];
@@ -165,8 +213,8 @@ int EvalPosition(const board_representation *pos) {
   }
 
   pce = wQ;
-  for (pceNum = 0; pceNum < pos->pieceNumber[pce]; ++pceNum) {
-    sq = pos->pieceList[pce][pceNum];
+  for (pieceNumber = 0; pieceNumber < pos->pieceNumber[pce]; ++pieceNumber) {
+    sq = pos->pieceList[pce][pieceNumber];
     ASSERT(SqOnBoard(sq));
     ASSERT(SQ64(sq) >= 0 && SQ64(sq) <= 63);
     ASSERT(FileRankValid(FilesBrd[sq]));
@@ -178,8 +226,8 @@ int EvalPosition(const board_representation *pos) {
   }
 
   pce = bQ;
-  for (pceNum = 0; pceNum < pos->pieceNumber[pce]; ++pceNum) {
-    sq = pos->pieceList[pce][pceNum];
+  for (pieceNumber = 0; pieceNumber < pos->pieceNumber[pce]; ++pieceNumber) {
+    sq = pos->pieceList[pce][pieceNumber];
     ASSERT(SqOnBoard(sq));
     ASSERT(SQ64(sq) >= 0 && SQ64(sq) <= 63);
     ASSERT(FileRankValid(FilesBrd[sq]));
@@ -189,28 +237,28 @@ int EvalPosition(const board_representation *pos) {
       score -= QueenSemiOpenFile;
     }
   }
-  // 8/p6k/6p1/5p2/P4K2/8/5pB1/8 b - - 2 62
+
   pce = wK;
   sq = pos->pieceList[pce][0];
   ASSERT(SqOnBoard(sq));
   ASSERT(SQ64(sq) >= 0 && SQ64(sq) <= 63);
 
-  // if ((pos->material[BLACK] <= ENDGAME_MAT)) {
-  //   score += KingE[SQ64(sq)];
-  // } else {
-  //   score += KingO[SQ64(sq)];
-  // }
+  if ((pos->material[BLACK] <= ENDGAME_MAT)) {
+    score += KingE[SQ64(sq)];
+  } else {
+    score += KingO[SQ64(sq)];
+  }
 
   pce = bK;
   sq = pos->pieceList[pce][0];
   ASSERT(SqOnBoard(sq));
   ASSERT(MIRROR64(SQ64(sq)) >= 0 && MIRROR64(SQ64(sq)) <= 63);
 
-  // if ((pos->material[WHITE] <= ENDGAME_MAT)) {
-  //   score -= KingE[MIRROR64(SQ64(sq))];
-  // } else {
-  //   score -= KingO[MIRROR64(SQ64(sq))];
-  // }
+  if ((pos->material[WHITE] <= ENDGAME_MAT)) {
+    score -= KingE[MIRROR64(SQ64(sq))];
+  } else {
+    score -= KingO[MIRROR64(SQ64(sq))];
+  }
 
   if (pos->pieceNumber[wB] >= 2)
     score += BishopPair;
